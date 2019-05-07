b041b41ff54e3bcadd68462469d20934dc954f49
Tarun Nagpal <tarun.nagpal@trantorinc.com>
2019-05-07 14:32:03 +0530

    Updating the Css and JS files
    Updating the Css and JS files

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
 <head>
 <title>ngaituk online</title>
 <meta name="viewport" content="width=device-width, initial-scale=1"/>
-<link rel="stylesheet" href="./css/style.css?version=51" type="text/css" />
+<link rel="stylesheet" href="css/style.css?version=51" type="text/css" />
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"/>
-<link rel="stylesheet" href="menufiles\mbcsmb1uu0.css?version=51" type="text/css" />
+<link rel="stylesheet" href="menufiles/mbcsmb1uu0.css?version=51" type="text/css" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
 <script type="text/javascript" src="menufiles/mbjsmb1uu0.js"></script>
 
