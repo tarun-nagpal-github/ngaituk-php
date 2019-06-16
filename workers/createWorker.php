55df43063eefcdf16190f5e6231d62e8d2991b38
Tarun Nagpal <tarun.nagpal@trantorinc.com>
2019-06-16 11:04:09 +0530

    Added Feature

--- workers/createWorker.php
+++ workers/createWorker.php
@@ -1,22 +1,18 @@
 <?php
   include "../config.php"; 
-  $data = json_decode(file_get_contents("php://input"));
-
   
-  echo json_encode($data);
-  die();
+$json = file_get_contents('php://input');
+$data = json_decode($json);
+
+ 
 
   $sql = "INSERT INTO workers (f_name, l_name, contractor_id, e_id,   v_type,  v_exp, d_o_b)
-               VALUES ('$data->f_name','$data->l_name','$data->contractor_id','$data->e_id',
-                      '$data->v_type','$data->v_exp','$data->d_o_b')";
+               VALUES ('$data->firstName','$data->lastName','$data->contractor','$data->employeeId',
+                      '$data->visaType','$data->visaExpiry','$data->dateOfBirth')";
 
-                      echo $sql;
+                      
         if($db->query($sql)===TRUE)
         {
           echo "data inserted" ;
           //echo $wrknm;
         }
-
-
-  
-  
