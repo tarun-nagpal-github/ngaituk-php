168b9b1bcda37844f56aa2d1653c18445932e06b
Tarun Nagpal <tarun.nagpal@trantorinc.com>
2019-06-16 16:45:51 +0530

    Create Worker Updated

--- workers/createWorker.php
+++ workers/createWorker.php
@@ -6,17 +6,23 @@ $data = json_decode($json);
 
  
 
-  $sql = "INSERT INTO workers (f_name, l_name, contractor_id, e_id,   v_type,  v_exp, d_o_b)
+if(
+    !empty($data->firstName) &&
+    !empty($data->contractor) &&
+    !empty($data->visaExpiry) &&
+    !empty($data->visaType)
+) 
+ {
+
+$query = "INSERT INTO workers (f_name, l_name, contractor_id, e_id,   v_type,  v_exp, d_o_b)
                VALUES ('$data->firstName','$data->lastName','$data->contractor','$data->employeeId',
                       '$data->visaType','$data->visaExpiry','$data->dateOfBirth')";
+$myArray = array(); 
 
-            $myArray = array();          
-        if($db->query($sql)===TRUE)
-        {
-          echo "data inserted" ;
-          //echo $wrknm;
-          $myArray[] = "saved";
-        }
-
-        
-        echo json_encode($myArray); 
+if ($result = $db->query($query)) {
+	print $result;
+    $myArray[] = $result;
+    echo json_encode($myArray);
+} 
+}
+$db->close();
