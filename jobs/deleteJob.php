db8c5e40a23ca5070291b9d936135c4ac2e5a977
Tarun Nagpal <tarun.nagpal@trantorinc.com>
2019-07-15 23:28:55 +0530

    Added Files

--- jobs/deleteJob.php
+++ jobs/deleteJob.php
@@ -2,15 +2,15 @@
   include "../config.php";   
 $json = file_get_contents('php://input');
 $data = json_decode($json);
-      $id =  htmlspecialchars($_GET["id"]);
+$s_no =  htmlspecialchars($_GET["s_no"]);
 
 
 
 if(
-    !empty($id) 
+    !empty($s_no) 
 ) 
  { 
- 	$query = "DELETE from `workers` WHERE id=$id";  
+ 	$query = "DELETE from `job_code` WHERE s_no=$s_no";  
 $myArray = array();
 if ($result = $db->query($query)) {     
     http_response_code(200);
