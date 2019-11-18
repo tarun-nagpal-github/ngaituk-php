583808f125ab1e5d36e2de0ab7df6ee6879da1e9
Tarun Nagpal <tarun.nagpal78@gmail.com>
2019-11-18 19:16:05 +0530

    Added the Unique Emp_Id for Worker

--- workers/createWorker.php
+++ workers/createWorker.php
@@ -9,8 +9,21 @@ if(
     !empty($data->visaType)
 ) 
  {
+
+  // Get the last inserted ID
+  $getLastIdQuery =  " SELECT MAX(id) as id FROM `workers` ";
+  if ($result = $db->query($getLastIdQuery)) { 
+      $e_id =  $result->fetch_object()->id;
+  }  else {
+    // Get the random number if SQL Fails
+     $e_id =  rand ( 200 , 999 );
+  }
+
+ // Emp ID Generated 
+ $e_id_new = strtoupper(substr($data->firstName, 0, 1).substr($data->lastName, 0, 1)."000".$e_id);  
+
 $query = "INSERT INTO workers (f_name, l_name, contractor_id, e_id,   v_type,  v_exp, d_o_b)
-               VALUES ('$data->firstName','$data->lastName','$data->contractor','$data->employeeId',
+               VALUES ('$data->firstName','$data->lastName','$data->contractor','$e_id_new',
                       '$data->visaType','$data->visaExpiry','$data->dateOfBirth')";
 $myArray = array(); 
 // print_r($query); die();
@@ -23,4 +36,8 @@ if ($result = $db->query($query)) {
   http_response_code(400);
 }
 }
+
+
+
+
 $db->close();
