a2a19a6e8b97edf0b3064bcc650e258ca0e4f2b0
Tarun Nagpal <tarun.nagpal78@gmail.com>
2019-12-04 13:09:17 +0530

    Updated the Save TimeSheet API

--- timesheet/saveTimesheet.php
+++ timesheet/saveTimesheet.php
@@ -1,35 +1,43 @@
 <?php
 include "../config.php"; 
 $json = file_get_contents('php://input');
-$data = json_decode($json);
-// print "<pre>";
-//  print_r($data);
-// print "</pre>";
+$data = json_decode($json); 
 
-// die("DATA FINALLY"); 
+
+ ini_set('display_errors',1);
+error_reporting(E_ALL);
 
 try {
-		foreach ($data as &$value) {  
-		       $query = "INSERT INTO timesheet 
-		                		(e_id,		  t_date,       crw_no, f_name, l_name, s_time, f_time, n_lunch, j_code, t_hrs, notes)
-		                 VALUES 
-		                 		( $value->id, $value->date, $value->f_name, $value->l_name, $value->id, $value->id, $value->id, $value->id, $value->id, $value->id, $value->id)"; 
-		       $result = $db->query($query);
-		}
-		echo json_encode(array(123 => 123 ));
-		http_response_code(200);
-}
+if(is_array($data)) {
 
+		foreach ($data as $key=>$value) {  
+	       $query = "INSERT INTO `timesheet`
+	                		(e_id, t_date, crw_no, f_name, l_name, s_time, f_time, n_lunch, j_code, t_hrs, notes)
+	                 VALUES 
+	                 		( '$value->id', '$value->date', '$value->e_id', '$value->f_name', '$value->l_name', '$value->startTime', '$value->endTime', 1, '$value->jobCode', '$value->hours', 1)"; 
+	                 		// print_r($query); 
+	                 		// $result = mysql_query($query) or trigger_error(mysql_error()." ".$query);
+
+		        $result = $db->query($query);	
+
+		        // print "RESULT";
+		        // var_dump($db);  
+		        // print "RESULT";
+		        // die("FINAL ONE");
+		}
+		echo json_encode(array('message' => 'data saved', 'result' => $result));
+		http_response_code(200);
+	} else {
+		print "<pre>";
+var_dump($data);
+print "</pre>";
+
+		die("NO DATA");
+	}
+} 
 //catch exception
 catch(Exception $e) {
-  echo 'Message: ' .$e->getMessage();
-  echo json_encode(array(123 => 123 ));
+  echo 'Message: ' .$e->getMessage();  
   http_response_code(400);
 }
-
-
-
- 
-
-
 $db->close();
